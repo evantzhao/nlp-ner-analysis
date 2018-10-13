4dbad54b231aecfab27c19303532565ce7076143
Evan Zhao <etz3@cornell.edu>
2018-10-13 03:18:09 -0400

    Updated file structure

---
+++ src/hmm.py
@@ -0,0 +1,9 @@
+class HiddenMarkovModel:
+	def __init__(self):
+		pass
+
+def main():
+	print("hmm run")
+
+if __name__ == '__main__':
+    main()
---
+++ src/memm.py
@@ -0,0 +1,9 @@
+class MaxEntMarkovModel:
+	def __init__(self):
+		pass
+
+def main():
+	print("memm run")
+
+if __name__ == '__main__':
+    main()
